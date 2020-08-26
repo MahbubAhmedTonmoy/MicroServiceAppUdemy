e498285a5e838351de4ea3e668708bff554e71ff
MahbubAhmedTonmoy <38882962+MahbubAhmedTonmoy@users.noreply.github.com>
2020-08-26 19:34:35 +0600

    rabbitMQ eventbus

---
+++ MicroServicesApp/src/Common/EventBusRabbitMQ/Producer/EventBusRabbitMQProducer.cs
@@ -0,0 +1,41 @@
+﻿using EventBusRabbitMQ.Events;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace EventBusRabbitMQ
+{
+    public class EventBusRabbitMQProducer
+    {
+        private readonly IRabbitMQConnection _rabbitMQConnection;
+        public EventBusRabbitMQProducer(IRabbitMQConnection rabbitMQConnection)
+        {
+            _rabbitMQConnection = rabbitMQConnection;
+        }
+
+        public void PublishBasketCheckOut(string queueName, BasketCheckoutEvent basketCheckoutEvent)
+        {
+            using (var channel = _rabbitMQConnection.CreateModel())
+            {
+                channel.QueueDeclare(queue: queueName, durable: false, autoDelete: false, arguments: null);
+                var message = JsonConvert.SerializeObject(basketCheckoutEvent);
+                var body = Encoding.UTF8.GetBytes(message);
+
+                IBasicProperties basicProperties = channel.CreateBasicProperties();
+                basicProperties.Persistent = true;
+                basicProperties.DeliveryMode = 2;
+
+                channel.ConfirmSelect();
+                channel.BasicPublish("", queueName, true, basicProperties, body);
+                channel.WaitForConfirmsOrDie();
+
+                channel.BasicAcks += (sender, eventArgs) => {
+                    Console.WriteLine("Sent RabbitMQ");
+                    //implement ack handle
+                };
+                channel.ConfirmSelect();
+            }
+        }
+    }
+}
---
+++ MicroServicesApp/src/Common/EventBusRabbitMQ/RabbitMQConnection.cs
@@ -0,0 +1,74 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading;
+
+namespace EventBusRabbitMQ
+{
+    public class RabbitMQConnection : IRabbitMQConnection
+    {
+        private readonly IConnectionFactory _connectionFactory;
+        private IConnection _connection;
+        private bool _dispose;
+        public RabbitMQConnection(IConnectionFactory connectionFactory)
+        {
+            _connectionFactory = connectionFactory;
+            if (!IsConnected)
+            {
+                TryConnect();
+            }
+        }
+        public bool TryConnect()
+        {
+            try
+            {
+                _connection = _connectionFactory.CreateConnection();
+            }
+            catch (BrokerUnreachableException)
+            {
+                Thread.Sleep(2000);
+                _connection = _connectionFactory.CreateConnection();
+            }
+            if (IsConnected)
+            {
+                return true;
+            }
+            else { return false; }
+        }
+
+        public bool IsConnected
+        {
+            get
+            {
+                return _connection != null && _connection.IsOpen && !_dispose;
+            }
+        }
+        public IModel CreateModel()
+        {
+            if (!IsConnected)
+            {
+                throw new InvalidOperationException("No RabbitMQ connections are available to perform this action");
+            }
+            return _connection.CreateModel();
+        }
+
+        public void Dispose()
+        {
+            if (_dispose) return;
+            _dispose = true;
+            try
+            {
+                _connection.Dispose();
+            }
+            catch(IOException ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        
+    }
+}
