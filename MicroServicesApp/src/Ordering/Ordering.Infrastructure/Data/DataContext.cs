dfca26bea10b884b105477102a58eb4584612d8e
MahbubAhmedTonmoy <38882962+MahbubAhmedTonmoy@users.noreply.github.com>
2020-08-27 15:25:03 +0600

    core infa added

---
+++ MicroServicesApp/src/Ordering/Ordering.Infrastructure/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ordering.Infrastructure.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+        public DbSet<Order> Orders { get; set; }
+    }
+}
