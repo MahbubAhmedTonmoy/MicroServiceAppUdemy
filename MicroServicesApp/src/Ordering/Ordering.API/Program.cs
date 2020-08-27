183b1bed1c50ec27f2b270c805ce9692d05edfb5
MahbubAhmedTonmoy <38882962+MahbubAhmedTonmoy@users.noreply.github.com>
2020-08-27 20:09:22 +0600

    order db set

--- MicroServicesApp/src/Ordering/Ordering.API/Program.cs
+++ MicroServicesApp/src/Ordering/Ordering.API/Program.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Ordering.Infrastructure.Data;
 
 namespace Ordering.API
 {
@@ -13,7 +15,28 @@ namespace Ordering.API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            CreateAndSeedDB(host);
+            host.Run();
+        }
+
+        private static void CreateAndSeedDB(IHost host)
+        {
+            using(var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var dbContext = services.GetRequiredService<DataContext>();
+                    OrderSeedData.SeedAsync(dbContext);
+                }
+                catch (Exception ex)
+                {
+
+                    throw;
+                }
+
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
---
+++ MicroServicesApp/src/Ordering/Ordering.Infrastructure/Data/OrderSeedData.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using Ordering.Core.Entities;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Data
+{
+    public class OrderSeedData
+    {
+        public static async Task SeedAsync(DataContext dataContext,int? retray = 0)
+        {
+            int retryForAvailability = retray.Value;
+
+            try
+            {
+                dataContext.Database.Migrate();
+                if (!dataContext.Orders.Any())
+                {
+                    dataContext.Orders.AddRange(PrepareSeed());
+                    await dataContext.SaveChangesAsync();
+                }
+            }
+            catch(Exception ex)
+            {
+                if (retryForAvailability < 5)
+                {
+                    retryForAvailability++;
+                    await SeedAsync(dataContext, retryForAvailability);
+                }
+                throw;
+            }
+        }
+
+        private static IEnumerable<Order> PrepareSeed()
+        {
+            return new List<Order>()
+            {
+                new Order() { UserName = "swn", FirstName = "Mehmet", LastName = "Ozkaya", EmailAddress = "meh@ozk.com", AddressLine = "Bahcelievler", TotalPrice = 5239 },
+                new Order() { UserName = "swn", FirstName = "Selim", LastName = "Arslan", EmailAddress ="sel@ars.com", AddressLine = "Ferah", TotalPrice = 3486 }
+            };
+        }
+    }
+}
