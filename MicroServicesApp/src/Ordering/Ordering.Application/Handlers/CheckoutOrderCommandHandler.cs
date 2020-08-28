fe94aab2c877bbd3659ecbeca62a4292564f84ea
MahbubAhmedTonmoy <38882962+MahbubAhmedTonmoy@users.noreply.github.com>
2020-08-28 15:09:53 +0600

    mapping post request done

--- MicroServicesApp/src/Ordering/Ordering.Application/Handlers/CheckoutOrderCommandHandler.cs
+++ MicroServicesApp/src/Ordering/Ordering.Application/Handlers/CheckoutOrderCommandHandler.cs
@@ -2,6 +2,7 @@
 using FluentValidation;
 using MediatR;
 using Ordering.Application.Commands;
+using Ordering.Application.Mapper;
 using Ordering.Application.Responses;
 using Ordering.Core.Entities;
 using Ordering.Core.Repository;
@@ -24,11 +25,11 @@ namespace Ordering.Application.Handlers
         }
         public async Task<OrderResponse> Handle(CheckoutOrderCommand request, CancellationToken cancellationToken)
         {
-            var order = _mapper.Map<Order>(request);
+            var order = OrderMapper.Mapper.Map<Order>(request);
             if (order == null)
                 throw new Exception("failed to map order");
             var newOrder = await _orderRepository.AddAsync(order);
-            var orderResponse = _mapper.Map<OrderResponse>(newOrder);
+            var orderResponse = OrderMapper.Mapper.Map<OrderResponse>(newOrder);
             return orderResponse;
         }
     }
